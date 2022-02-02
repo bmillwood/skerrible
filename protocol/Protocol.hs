f621b05825c1fef73ef29ded57358e4a1f00de3f
Ben Millwood <thebenmachine+git@gmail.com>
2022-02-02 12:07:07 -0800

    some game types

--- protocol/Protocol.hs
+++ protocol/Protocol.hs
@@ -4,12 +4,48 @@
 module Protocol where
 
 import qualified Data.Aeson as Aeson
+import Data.Map (Map)
 import Data.Set (Set)
 import Data.Text (Text)
 import GHC.Generics
 
 portNumber :: (Num n) => n
 portNumber = 4170
+
+data Tile =
+  Tile
+    { tileChar :: Char
+    , tileScore :: Rational
+    } deriving (Generic, Show)
+
+instance Aeson.FromJSON Tile
+instance Aeson.ToJSON Tile
+
+data Square =
+  Square
+    { letterMult :: Rational
+    , wordMult :: Rational
+    , squareTile :: Tile
+    } deriving (Generic, Show)
+
+instance Aeson.FromJSON Square
+instance Aeson.ToJSON Square
+
+data Pos = Pos Integer Integer
+  deriving (Eq, Generic, Ord, Show)
+
+instance Aeson.FromJSON Pos
+instance Aeson.ToJSON Pos
+
+-- these instances are arguably not optimal, but they sure are convenient
+instance Aeson.ToJSONKey Pos
+instance Aeson.FromJSONKey Pos
+
+newtype Board = Board (Map Pos Square)
+  deriving (Generic, Show)
+
+instance Aeson.FromJSON Board
+instance Aeson.ToJSON Board
 
 data FromClient
   = LoginRequest { loginRequestName :: Text }
@@ -22,6 +58,7 @@ instance Aeson.ToJSON FromClient
 data ToClient
   = Folks { loggedInOthers :: Set Text }
   | Message { msgSentBy :: Text, msgContent :: Text }
+  | UpdateBoard Board
   deriving (Generic, Show)
 
 instance Aeson.FromJSON ToClient
