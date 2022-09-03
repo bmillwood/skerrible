614d782d867fa7e5513def5ddf7f604ad1bb64fa
Ben Millwood <thebenmachine+git@gmail.com>
2022-09-03 18:04:59 +0100

    changes to skerrible.cabal retrigger build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ killGrandchild() {
 
 while sleep 1
 do
-  inotifywait --quiet -e modify -e delete protocol server/{src,tests} &
+  inotifywait --quiet -e modify -e delete protocol server/{src,tests} skerrible.cabal &
   if cabal build "$@"
   then
     cabal run skerrible-server -- static-root &
