1fce2fbe5fcc687e0ceea5fdbce083cc5345ff12
Ben Millwood <thebenmachine+git@gmail.com>
2023-01-28 15:53:46 +0000

    terraform also output zone_id

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "dns_name" {
   value = aws_lb.skerrible.dns_name
 }
+
+output "zone_id" {
+  value = aws_lb.skerrible.zone_id
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "desired_count" {
   type = number
 }
+
 variable "internal_port" {
   type = number
   default = 4170
